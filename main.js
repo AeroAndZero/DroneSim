7eb02717abefe4e06edbb285e43a24e57231d198
Ayush Thakur <ayushthakur667@gmail.com>
2023-04-14 00:45:11 +0530

    Basic Drone Scene
    - Added drone model
    - Added HDRI images and Sphere
    - Added Ambient Lighting
    - Improved grouping of objects

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
 import * as THREE from 'three';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // setting up scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // setting up renderer
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("main")});
 renderer.setSize(window.innerWidth, window.innerHeight);
 let renderList = [];
 
-// main code
-let fps = 0; 
+// importing models
+const drone = new GLTFLoader();
+drone.load('res/DJI Mini 3.glb', function(gltf){
+    console.log(gltf);
+    scene.add(gltf.scene);
+}, undefined, function(error){
+    console.error(error);
+});
 
+
+/*
+*       MAIN CODE
+*/
+
+// lights, camera..
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
 camera.position.set(0,2,5);
 camera.rotateOnAxis(new THREE.Vector3(1,0,0), -0.3);
 
-let cube = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshStandardMaterial({color: 0x00ff00}))
-cube.position.y += 0.5;
-renderList.push(cube);
+const ambientLight = new THREE.AmbientLight( 0xFFFFFF,1.6 );
+renderList.push( ambientLight );
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(1,2,1);
 renderList.push(pointLight);
+
+const hdriTex = new THREE.TextureLoader().load( 'res/kloppenheim_05_puresky.webp' );
+const skybox = new THREE.Mesh(new THREE.SphereGeometry(900, 32, 32), new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide, map: hdriTex}));
+renderList.push(skybox);
 
 const lightHelper = new THREE.PointLightHelper(pointLight, 0.2);
 renderList.push(lightHelper);
@@ -29,12 +46,17 @@ renderList.push(lightHelper);
 const gridHelper = new THREE.GridHelper(20,40);
 renderList.push(gridHelper);
 
-// adding to the render list
+// ...action!
+const controls = new OrbitControls( camera, renderer.domElement );
+console.log(drone)
+
+// Adding elements to the scene
 renderList.forEach((obj) => {
     scene.add(obj);
 })
 
-// animating
+// Rendering every frame
+let fps = 0;
 function animate(){
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
